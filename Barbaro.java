c4a8b80c998826e601d0e2068ac2373906db03fb
RamiroNB <ramiro.nilson.barros@gmail.com>
2022-11-28 15:44:48 -0300

    Add Barbarian Class

---
+++ Barbaro.java
@@ -0,0 +1,22 @@
+public class Barbaro{
+
+    private int terras;
+    private String nome;
+
+
+    public Barbaro(String name, int land){
+        this.nome =name;
+        this.terras = land;
+    }
+
+    public int getTerras() {
+        return terras;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    
+
+}
